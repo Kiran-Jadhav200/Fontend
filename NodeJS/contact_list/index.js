c83e0d5d66fa24126b65b51d88df30654e17c791
Kiran Jadhav <jadhavrkiran21@gmail.com>
2023-10-04 19:18:29 +0530

    ejs

--- NodeJS/contact_list/index.js
+++ NodeJS/contact_list/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
 app.get('/',function(req,res){
-    res.send('<h1>Cool, it is running!</h1>');
-    console.log(req);
+   
+  return res.render('home');
+
 });
 
 
